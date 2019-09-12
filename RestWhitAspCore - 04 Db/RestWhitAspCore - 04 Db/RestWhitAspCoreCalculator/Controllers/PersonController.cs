3eb6662763a26c75c0da6979d3fe772e51100bac
Igor Leroy <igor_leroy@gmail.com>
2019-09-11 22:18:48 -0300

    Adicionado controle de versão, arquitetura para persistencia no Banco de Dados, CRUD.

---
+++ RestWhitAspCore - 04 Db/RestWhitAspCore - 04 Db/RestWhitAspCoreCalculator/Controllers/PersonController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RestWhitAspCoreUdemy.Services;
+using RestWhitAspCoreUdemy.Model;
+
+namespace RestWhitAspCoreUdemy.Controllers
+{
+    [ApiVersion("1")]
+    [Route("api/[controller]/v{version:apiVersion}")]
+    public class PersonController : Controller
+    {
+        private IPersonService _personService;
+
+        public PersonController(IPersonService personService)
+        {
+            _personService = personService;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(_personService.FindAll());
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            var person = _personService.FindById(id);
+                if (person == null) return NotFound();
+                return Ok(person);
+        }
+
+        // POST api/values
+        [HttpPost]
+        public IActionResult Post([FromBody]Person person)
+        {
+            if (person == null) return BadRequest();
+            return new ObjectResult(_personService.Create(person));
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public IActionResult Put([FromBody]Person person)
+        {
+            if (person == null) return BadRequest();
+            return new ObjectResult(_personService.Update(person));
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            _personService.Delete(id);
+            return NoContent();
+        }
+    }
+}
