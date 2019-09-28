33ab21522f04f5103fea308a5c85d6777108b238
Igor Leroy <igor_leroy@gmail.com>
2019-09-28 15:12:57 -0300

    Projeto Migrations

---
+++ RestWhitAspCore - 06 Migrations/RestWhitAspCore - 06 Migrations/RestWhitAspCoreCalculator/Business/Implemetation/PersonBusinessImpl.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using RestWhitAspCoreUdemy.Model;
+using RestWhitAspCoreUdemy.Model.Context;
+using RestWhitAspCoreUdemy.Repository;
+
+namespace RestWhitAspCoreUdemy.Business.Implemetation
+{
+    public class PersonBusinessImpl : IPersonBusiness
+    {
+        private IPersonRepository _repository;
+
+        public PersonBusinessImpl(IPersonRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public Person Create(Person person)
+        {
+            return _repository.Create(person);
+        }
+
+        public Person Update(Person person)
+        {
+            return _repository.Update(person);
+        }
+
+        public Person FindById(long id)
+        {
+            return _repository.FindById(id);
+        }
+
+        public List<Person> FindAll()
+        {
+            return _repository.FindAll();
+        }
+
+        public void Delete(long id)
+        {
+            _repository.Delete(id);
+        }
+    }
+}
---
+++ RestWhitAspCore - 06 Migrations/RestWhitAspCore - 06 Migrations/RestWhitAspCoreCalculator/Startup.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using RestWhitAspCoreUdemy.Model.Context;
+using RestWhitAspCoreUdemy.Business;
+using RestWhitAspCoreUdemy.Business.Implemetation;
+using RestWhitAspCoreUdemy.Repository;
+using RestWhitAspCoreUdemy.Repository.Implemetation;
+
+namespace RestWhitAspCoreCalculator
+{
+    public class Startup
+    {
+        private readonly ILogger _logger;
+        public IConfiguration _configuration { get; }
+        public IHostingEnvironment _enviroment { get; }
+        public Startup(IConfiguration configuration, IHostingEnvironment environment, ILogger<Startup> logger)
+        {
+            _configuration = configuration;
+            _enviroment = environment;
+            _logger = logger;
+        }
+
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            var connectionString = _configuration["MysqlConnection:MySqlConnectionString"];
+            services.AddDbContext<MySQLContext>(options => options.UseMySql(connectionString));
+
+            try
+            {
+                var envolveConnection = new MySql.Data.MySqlClient.MySqlConnection(connectionString);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogCritical("Database migration failed", ex);
+                throw;
+            }
+
+            services.AddMvc();
+
+            services.AddApiVersioning();
+
+            services.AddScoped<IPersonBusiness, PersonBusinessImpl>();
+            services.AddScoped<IPersonRepository, PersonRepositoryImpl>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseMvc();
+        }
+    }
+}
